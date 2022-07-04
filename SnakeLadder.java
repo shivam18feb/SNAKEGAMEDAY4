4a962a0ff9670a8fe1d139c0d5655fe88c06cc27
unknown <shivam18feb@gmail.com>
2022-07-04 12:54:57 +0530

    palyerthencheck

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,24 @@
 package snakeandladder;
 
 public class SnakeLadder {
+	public static final int START_POSITION = 0;
+	public static final int IS_SNAKE = 1;
+	public static final int IS_LADDER = 2;
+	
+	
+	
 	public static void main(String[] args) {
-		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		
-		int currentPosition = 0;
-		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
-		System.out.println("Dice Number : " + diceNumber);
+		
+		int option = (int) ((Math.random() * 10 % 3) + 1); 
+		if (option == IS_SNAKE) {
+			currentPosition -= diceNumber;
+			System.out.println("Player in snake position : " + currentPosition);
+		} else if (option == IS_LADDER) {
+			currentPosition += diceNumber;
+			System.out.println("Player in ladder position : " + currentPosition);
+		} else {
+			System.out.println("No Play " + currentPosition);
+		}
 	}
 }
