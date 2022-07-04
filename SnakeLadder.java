9751b2f6209f97670ef3c5b8a1d52122bcc747e9
unknown <shivam18feb@gmail.com>
2022-07-04 12:59:43 +0530

    Repeat the player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,15 +4,23 @@ public class SnakeLadder {
 	public static final int START_POSITION = 0;
 	public static final int IS_SNAKE = 1;
 	public static final int IS_LADDER = 2;
-	
-	
-	
+
 	public static void main(String[] args) {
+		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
+
+		
+		int currentPosition = 0;
+		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
+		System.out.println("Dice Number : " + diceNumber);
+		
 		
 		
 		int option = (int) ((Math.random() * 10 % 3) + 1); 
 		if (option == IS_SNAKE) {
 			currentPosition -= diceNumber;
+			if (currentPosition < 0) { 
+				currentPosition = START_POSITION;
+			}
 			System.out.println("Player in snake position : " + currentPosition);
 		} else if (option == IS_LADDER) {
 			currentPosition += diceNumber;
