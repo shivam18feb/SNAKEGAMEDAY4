321465a92cfc129499f331b8fbae19b648b87cf0
unknown <shivam18feb@gmail.com>
2022-07-04 12:49:23 +0530

    Rolldice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,8 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		
-		int currentPosition = 0; 
+		int currentPosition = 0;
+		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
+		System.out.println("Dice Number : " + diceNumber);
 	}
 }
